4f2bdbc2a5ed78a29c1e6c9691cc53dd7ec77e14
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-04-03 16:08:07 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+    const userDetails = {
+        name: "name",
+        email: "email",
+    };
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userDetails)
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => document.body.innerHTML = data.id)
+      .catch(error => document.body.innerHTML = error.message);
+}
+submitData("Steve", "steve@steve.com");
